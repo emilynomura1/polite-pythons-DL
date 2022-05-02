ef58fc8b94b0127bd8db40f0b235f7dc6bff882f
emilynomura1 <emily_nomura@brown.edu>
2022-05-02 17:47:10 -0400

    use enumerate to replace

--- code/tokenize_UNK.py
+++ code/tokenize_UNK.py
@@ -1,3 +1,4 @@
+import numpy as np
 from collections import Counter
 from change_data import change_data
 
@@ -7,11 +8,12 @@ non_polite_train, polite_train, non_polite_test, polite_test = change_data()
 flat_list = [item for sublist in non_polite_train for item in sublist]
 x = Counter(flat_list)
 words_to_remove = dict((k,v) for k,v in x.items() if v==1)
-# print(len(words_to_remove))
+print(len(words_to_remove))
 non_polite_train_omit_words = list(words_to_remove.keys())
+print(non_polite_train_omit_words[0:100])
 # Actually replace words in non_polite_train with '*UNK*' if they appear in non_polite_train_omit_words
-for sentence in non_polite_train:
-    for word in sentence:
+for i, sentence in np.ndenumerate(non_polite_train):
+    for j, word in enumerate(sentence):
         if word in non_polite_train_omit_words:
-            word='*UNK*'
-print(non_polite_train[0:50])
+            non_polite_train[i][j] = '*UNK*'
+print(non_polite_train[0:100])

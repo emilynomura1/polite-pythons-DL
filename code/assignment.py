add more dense layers & hidden units, fix bleu score, plot losses

code/assignment.py
@@ -1,79 +1,65 @@
+from distutils.command.install_egg_info import to_filename
 import os
 import numpy as np
 import tensorflow as tf
 import numpy as np
 from preprocess import *
-#from transformer_model import Transformer_Seq2Seq
 from rnn_model import RNN_Seq2Seq
-import sys
-import random
+import matplotlib.pyplot as plt
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 
-#from attenvis import AttentionVis
-#av = AttentionVis()
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-def train(model, train_french, train_english, eng_padding_index, fr_padding_index):
+def train(model, non_polite_train, polite_train, polite_padding_index, non_polite_padding_index):
 	"""
 	Runs through one epoch - all training examples.
 	:param model: the initialized model to use for forward and backward pass
-	:param train_french: French train data (all data for training) of shape (num_sentences, window_size)
-	:param train_english: English train data (all data for training) of shape (num_sentences, window_size + 1)
-	:param eng_padding_index: the padding index, the id of *PAD* token. This integer is used when masking padding labels.
-	:return: None
+	:param non_polite_train: Non-polite train data (all data for training) of shape (num_sentences, window_size)
+	:param polite_train: Polite train data (all data for training) of shape (num_sentences, window_size + 1)
+	:param polite_padding_index: the padding index, the id of *PAD* token. This integer is used when masking padding labels.
+	:param non_polite_padding_index: the padding index, the id of the *PAD* token. This integer is used when masking attention.
+	:returns: a list of losses with length = total number of batches
 	"""
-	# NOTE: For each training step, you should pass in the French sentences to be used by the encoder,
-	# and English sentences to be used by the decoder
-	# - The English sentences passed to the decoder should have the last token in the window removed:
-	#	 [STOP CS147 is the best class. STOP *PAD*] --> [STOP CS147 is the best class. STOP]
-	#
-	# - When computing loss, the decoder labels should have the first word removed:
-	#	 [STOP CS147 is the best class. STOP] --> [CS147 is the best class. STOP]
 
 	# Format decoder inputs and decoder labels
-	decoder_input = [i[:-1] for i in train_english]#train_english[:-1]
-	decoder_label = [i[1:] for i in train_english]#train_english[1:]
-
-	# train_batch=0
+	decoder_input = [i[:-1] for i in polite_train]
+	decoder_label = [i[1:] for i in polite_train]
+	loss_list = []
 
 	# Batch training
-	for i in range(0, len(train_french), model.batch_size): #batch training
+	for i in range(0, len(non_polite_train), model.batch_size):
 
-		# train_batch+=1
-		# print(train_batch)
-
-		encoder_input_batch = train_french[i:i+model.batch_size]
+		encoder_input_batch = non_polite_train[i:i+model.batch_size]
 		decoder_input_batch = decoder_input[i:i+model.batch_size]
 		decoder_label_batch = decoder_label[i:i+model.batch_size]
 
 		# Create mask within for-loop
-		# mask = [i != eng_padding_index for i in decoder_label_batch]
-		input_mask = [np.where(np.array(i)==fr_padding_index, False, True) for i in encoder_input_batch]
-		mask = [np.where(np.array(i)==eng_padding_index, 0, 1) for i in decoder_label_batch]
+		input_mask = [np.where(np.array(i)==non_polite_padding_index, False, True) for i in encoder_input_batch] #boolean mask
+		mask = [np.where(np.array(i)==polite_padding_index, 0, 1) for i in decoder_label_batch] #binary mask
 
 		# Forward and backward pass
 		with tf.GradientTape() as tape:
 			probs = model.call(encoder_input_batch, decoder_input_batch, input_mask)
 			loss = model.loss_function(probs, decoder_label_batch, mask)
 		gradients = tape.gradient(loss, model.trainable_variables)
+		loss_list.append(loss)
 		model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+	return loss_list
 
-#@av.test_func
-def test(model, test_french, test_english, eng_padding_index, fr_padding_index):
+def test(model, non_polite_test, polite_test, polite_padding_index, non_polite_padding_index):
 	"""
 	Runs through one epoch - all testing examples.
 	:param model: the initialized model to use for forward and backward pass
-	:param test_french: French test data (all data for testing) of shape (num_sentences, window_size)
-	:param test_english: English test data (all data for testing) of shape (num_sentences, window_size + 1)
-	:param eng_padding_index: the padding index, the id of *PAD* token. This integer is used when masking padding labels.
+	:param non_polite_test: Non-polite test data (all data for testing) of shape (num_sentences, window_size)
+	:param polite_test: Polite test data (all data for testing) of shape (num_sentences, window_size + 1)
+	:param polite_padding_index: the padding index, the id of *PAD* token. This integer is used when masking padding labels.
+	:param non_polite_padding_index: the padding index, the id of *PAD* token. This integer is used when masking attention.
 	:returns: a tuple containing at index 0 the perplexity of the test set and at index 1 the per symbol accuracy on test set,
-	e.g. (my_perplexity, my_accuracy)
 	"""
-	# Note: Follow the same procedure as in train() to construct batches of data!
 
-	decoder_input = [i[:-1] for i in test_english]
-	decoder_label = [i[1:] for i in test_english]
+	decoder_input = [i[:-1] for i in polite_test]
+	decoder_label = [i[1:] for i in polite_test]
 
-	test_batch = 0
 	test_loss = 0
 	test_accuracy = 0
 	num_valid_total = 0
@@ -81,47 +67,42 @@ def test(model, test_french, test_english, eng_padding_index, fr_padding_index):
 	test_bleu = 0
 
 	# Batch testing
-	for i in range(0, len(test_french), model.batch_size):
-		test_batch += 1
-		# print(test_batch)
+	for i in range(0, len(non_polite_test), model.batch_size):
 
-		encoder_input_batch = test_french[i:i+model.batch_size]
+		encoder_input_batch = non_polite_test[i:i+model.batch_size]
 		decoder_input_batch = decoder_input[i:i+model.batch_size]
 		decoder_label_batch = decoder_label[i:i+model.batch_size]
 
 		# Create mask within for-loop
-		# mask = [i != eng_padding_index for i in decoder_label_batch]
-		
-		input_mask = [np.where(np.array(i)==fr_padding_index, False, True) for i in encoder_input_batch]
-		mask = [np.where(np.array(i)==eng_padding_index, 0, 1) for i in decoder_label_batch]
+		input_mask = [np.where(np.array(i)==non_polite_padding_index, False, True) for i in encoder_input_batch]
+		mask = [np.where(np.array(i)==polite_padding_index, 0, 1) for i in decoder_label_batch]
 
 		num_valid_per_batch = tf.math.reduce_sum(mask) #get number of valid tokens (non-pad values) per batch
 		num_valid_total += num_valid_per_batch #iteratively sum to get total number of valid tokens
 
 		# Call model, compute loss & accuracy
 		probs = model.call(encoder_input_batch, decoder_input_batch, input_mask)
-
 		test_loss += model.loss_function(probs, decoder_label_batch, mask)
 		test_accuracy = model.accuracy_function(probs, decoder_label_batch, mask)
-		# test_bleu += model.bleu_score(probs, decoder_label_batch, mask)
 		sum_acc += tf.math.multiply(test_accuracy, tf.cast(num_valid_per_batch, dtype=tf.float32))
+		# test_bleu += model.bleu_score(probs, decoder_label_batch, mask)
 
 	accuracy = tf.math.divide(sum_acc, tf.cast(num_valid_total, dtype=tf.float32))
-	# bleu = tf.math.divide(test_bleu, tf.cast(num_valid_total, dtype=tf.float32)) #total number of correctly predicted words/total number of valid tokens
 	perplexity = tf.math.exp(tf.math.divide(test_loss, tf.cast(num_valid_total, dtype=tf.float32))) #total loss/total number of valid tokens
+	# bleu = tf.math.divide(test_bleu, tf.cast(num_valid_total, dtype=tf.float32)) #total number of correctly predicted words/total number of valid tokens
 	
 	return perplexity, accuracy#, bleu
 
-def translate(model, sentence, vocab_frn, vocab_eng, fr_padding_index, toword):
+def translate(model, sentence, non_polite_vocab, polite_vocab, non_polite_padding_index, toword):
 	# padded, ignore1 = pad_corpus(sentence, None)
 	# tokenized = convert_to_id(vocab_frn, padded)
 	sentence = tf.convert_to_tensor(sentence)
 	sentence = tf.reshape(sentence, (1, len(sentence)))
 	encoded, state1 = model.encoder_gru(tf.nn.embedding_lookup(model.input_embedding, sentence))
-	decoder_sentence = tf.expand_dims([vocab_eng['*START*']], 0)
+	decoder_sentence = tf.expand_dims([polite_vocab['*START*']], 0)
 	result = []
-	input_mask = [np.where(np.array(i)==fr_padding_index, False, True) for i in sentence]
-	for i in range(model.english_window_size):
+	input_mask = [np.where(np.array(i)==non_polite_padding_index, False, True) for i in sentence]
+	for i in range(POLITE_WINDOW_SIZE):
 		#print(decoder_sentence)
 		output_embeddings = tf.nn.embedding_lookup(model.output_embedding, decoder_sentence)
 		gru2,state = model.decoder_gru(output_embeddings,initial_state = state1)
@@ -129,14 +110,16 @@ def translate(model, sentence, vocab_frn, vocab_eng, fr_padding_index, toword):
 		value_mask = tf.convert_to_tensor(input_mask, dtype=bool)
 		att_output = model.additive_attention([gru2,encoded], mask=[query_mask, value_mask], training=True, return_attention_scores=False)
 		dense1 = model.dense1(att_output)
-		logits = model.densef(dense1)
+		dense2 = model.dense2(dense1)
+		dense3 = model.dense3(dense2)
+		logits = model.densef(dense3)
 		prbs = tf.nn.softmax(logits) # (1, 1, n)
 		pred_id=np.random.choice(np.arange(prbs.shape[2]), p = np.asarray(prbs).flatten())
 		# print(type(pred_id))
-		# predicted_id = tf.argmax(prbs[0]).numpy()
+		# pred_id = tf.argmax(np.asarray(prbs).flatten()).numpy()
 		# print(type(prbs), prbs.shape)
 		# print(type(predicted_id, predicted_id.shape))
-		print(toword[pred_id])
+		#print(toword[pred_id])
 		result = result+ [toword[pred_id]]
 		if (toword[pred_id]=='*STOP*'):
 			return result
@@ -145,62 +128,70 @@ def translate(model, sentence, vocab_frn, vocab_eng, fr_padding_index, toword):
 	return result
 
 def main():
-	
-	#model_types = {"RNN" : RNN_Seq2Seq, "TRANSFORMER" : Transformer_Seq2Seq}
-	#if len(sys.argv) != 2 or sys.argv[1] not in model_types.keys():
-		#print("USAGE: python assignment.py <Model Type>")
-		#print("<Model Type>: [RNN/TRANSFORMER]")
-		#exit()
-
-	# Change this to "True" to turn on the attention matrix visualization.
-	# You should turn this on once you feel your code is working.
-	# Note that it is designed to work with transformers that have single attention heads.
-	#if sys.argv[1] == "TRANSFORMER":
-		#av.setup_visualization(enable=True)
 
 	print("Running preprocessing...")
-	#data_dir   = '../../data'
-	#file_names = ('fls.txt', 'els.txt', 'flt.txt', 'elt.txt')
-	#file_paths = [f'{data_dir}/{fname}' for fname in file_names]
-	train_eng,test_eng, train_frn,test_frn, vocab_eng,vocab_frn,eng_padding_index,fr_padding_index = get_data()
+	polite_train, polite_test, non_polite_train, non_polite_test, polite_vocab, non_polite_vocab, polite_padding_index, non_polite_padding_index = get_data()
 	print("Preprocessing complete.")
-	print(train_eng.shape, test_eng.shape, train_frn.shape, test_frn.shape)
-	model = RNN_Seq2Seq(52, len(vocab_frn), 52, len(vocab_eng))
-
-	# TODO:
-	# Train and Test Model for 1 epoch.
+	# print(polite_train.shape, polite_test.shape, non_polite_train.shape, non_polite_test.shape)
+	model = RNN_Seq2Seq(NON_POLITE_WINDOW_SIZE, len(non_polite_vocab), POLITE_WINDOW_SIZE, len(polite_vocab))
 
 	# Drop extra sentences since batch_size is not evenly divisible
-	# train_eng = train_eng[:-55]
-	# train_frn = train_frn[:-55]
-	# test_eng = test_eng[:-25]
-	# test_frn = test_frn[:-25]
+	polite_train = polite_train[:-55]
+	non_polite_train = non_polite_train[:-55]
+	polite_test = polite_test[:-25]
+	non_polite_test = non_polite_test[:-25]
 
 	# Run on a subset of data
-	train_eng = train_eng[0:50000]
-	train_frn = train_frn[0:50000]
-	test_eng = test_eng[0:1000]
-	test_frn = test_frn[0:1000]
+	# polite_train = polite_train[0:5000]
+	# non_polite_train = non_polite_train[0:5000]
+	# polite_test = polite_test[0:1000]
+	# non_polite_test = non_polite_test[0:1000]
 
 	print("train start")
-	train(model, train_frn, train_eng, eng_padding_index, fr_padding_index)
+	loss_list = train(model, non_polite_train, polite_train, polite_padding_index, non_polite_padding_index)
 	print("train finish")
+
+	# Print plot of losses over each batch
+	x = [i for i in range(len(loss_list))]
+	plt.plot(x, loss_list)
+	plt.title('Loss per batch')
+	plt.xlabel('Batch')
+	plt.ylabel('Loss')
+	plt.show()
 
 	# Save trained model
 	# model.save('../models/model2', save_format='tf')
+	
 
-
-	perplexity, accuracy = test(model, test_frn, test_eng, eng_padding_index, fr_padding_index)
+	perplexity, accuracy = test(model, non_polite_test, polite_test, polite_padding_index, non_polite_padding_index)
 	print("Perplexity: ", perplexity, "Accuracy: ", accuracy)
+	smooth = SmoothingFunction(epsilon = 1e-12).method1
+	# Test translator on one input sentence
+	#sentence = non_polite_test[5]
+	toword = {v: k for k, v in polite_vocab.items()}
+	bleu = []
+	true_sentence = []
+	pred_sentence = []
+	to_translate = non_polite_test[0:1]
+	p = polite_test[0:100]
+	for i in range(len(p)):
+		s = []
+		sentence = p[i]
+		non = to_translate[i]
+		for word in sentence:
+			if word != polite_padding_index:
+				s.append(toword[word])
+				print(word, toword[word])
+		result = translate(model, non, non_polite_vocab, polite_vocab, non_polite_padding_index, toword)
+		true_sentence.append(s)
+		pred_sentence.append(result)
+		bleu.append(sentence_bleu(s,result, smoothing_function = smooth))		
+				
 
-	sentence = test_frn[4]
-	toword = {v: k for k, v in vocab_eng.items()}
-	result = translate(model, sentence, vocab_frn, vocab_eng, fr_padding_index, toword)
-	print(result)
-
-	# Visualize a sample attention matrix from the test set
-	# Only takes effect if you enabled visualizations above
-	#av.show_atten_heatmap()
+	
+	print(np.mean(np.asarray(bleu)))
+	print(true_sentence[10], pred_sentence[10])
+	
 
 if __name__ == '__main__':
 	main()

code/preprocess.py
@@ -5,18 +5,15 @@ from collections import Counter
 #av = AttentionVis()
 from get_final_data import get_final_data
 
-##########DO NOT CHANGE#####################
 PAD_TOKEN = "*PAD*"
 STOP_TOKEN = "*STOP*"
 START_TOKEN = "*START*"
 UNK_TOKEN = "*UNK*"
 NON_POLITE_WINDOW_SIZE = 52
 POLITE_WINDOW_SIZE = 52
-##########DO NOT CHANGE#####################
 
 def pad_corpus(non_polite, polite):
     """
-	DO NOT CHANGE:
 	arguments are lists of non_polite, polite sentences. Returns [non_polite-sents, polite-sents]. The
 	text is given an initial "*STOP*". polite is padded with "*START*" at the beginning for Teacher Forcing.
 	:param non_polite: list of non_polite sentences
@@ -24,6 +21,7 @@ def pad_corpus(non_polite, polite):
 	:param polite: list of polite sentences
 	:return: A tuple of: (list of padded sentences for non_polite, list of padded sentences for polite)
 	"""
+
     non_polite_padded_sentences = []
     for line in non_polite:
         padded_non_polite = line[:NON_POLITE_WINDOW_SIZE]
@@ -38,89 +36,64 @@ def pad_corpus(non_polite, polite):
 
 def build_vocab(sentences):
     """
-	DO NOT CHANGE
     Builds vocab from list of sentences
 	:param sentences:  list of sentences, each a list of words
 	:return: tuple of (dictionary: word --> unique index, pad_token_idx)
     """
-    tokens = []
 
+    tokens = []
     for s in sentences: 
         tokens.extend(s)
     all_words = sorted(list(set([STOP_TOKEN,PAD_TOKEN,UNK_TOKEN] + tokens)))
-    # flat_list = [item for sublist in sentences for item in sublist]
-    
-    # x = Counter(flat_list)
-    # y = dict((k,v) for k,v in x.items() if v>2)
-    # keep_words = list(y.keys())
-    # all_words_new = []
-    # for word in all_words:
-    #     if word in keep_words:
-    #         all_words_new.append(word)
     vocab =  {word:i for i,word in enumerate(all_words)}
-    # vocab["*UNK*"] = len(vocab)
     return vocab, vocab[PAD_TOKEN]
 
 def convert_to_id(vocab, sentences):
     """
-	DO NOT CHANGE
     Convert sentences to indexed
 	:param vocab:  dictionary, word --> unique index
 	:param sentences:  list of lists of words, each representing padded sentence
 	:return: numpy array of integers, with each row representing the word indeces in the corresponding sentences
     """
-    # print(len(sentences), print(len(vocab)))
+
     return np.stack([[vocab[word] if word in vocab else vocab[UNK_TOKEN] for word in sentence] for sentence in sentences])
 
-#@av.get_data_func
 def get_data():
     """
 	Use the helper functions in this file to read and parse training and test data, then pad the corpus.
 	Then vectorize your train and test data based on your vocabulary dictionaries.
-	:param non_polite_training_file: Path to the non_polite training file.
-	:param polite_training_file: Path to the polite training file.
-	:param non_polite_test_file: Path to the non_polite test file.
-	:param polite_test_file: Path to the polite test file.
 	:return: Tuple of train containing:
-	(2-d list or array with polite training sentences in vectorized/id form [num_sentences x 15] ),
-	(2-d list or array with polite test sentences in vectorized/id form [num_sentences x 15]),
-	(2-d list or array with non_polite training sentences in vectorized/id form [num_sentences x 14]),
-	(2-d list or array with non_polite test sentences in vectorized/id form [num_sentences x 14]),
+	(2-d list or array with polite training sentences in vectorized/id form [num_sentences x POLITE_WINDOW_SIZE]),
+	(2-d list or array with polite test sentences in vectorized/id form [num_sentences x POLITE_WINDOW_SIZE]),
+	(2-d list or array with non_polite training sentences in vectorized/id form [num_sentences x NON_POLITE_WINDOW_SIZE]),
+	(2-d list or array with non_polite test sentences in vectorized/id form [num_sentences x NON_POLITE_WINDOW_SIZE]),
 	polite vocab (Dict containg word->index mapping),
 	non_polite vocab (Dict containg word->index mapping),
-	polite padding ID (the ID used for *PAD* in the polite vocab. This will be used for masking loss)\
+	polite padding ID (the ID used for *PAD* in the polite vocab. This will be used for masking loss)
+    non_polite padding ID (the ID used for *PAD* in the non-polite vocab. This will be used for masking attention)
     """
 
-	#1) Read tokenized polite and non_polite Data for training and testing
+	# Read tokenized polite and non_polite Data for training and testing
     non_polite_train, polite_train, non_polite_test, polite_test = get_final_data()
     
-	#2) Pad training data (see pad_corpus)
+	# Pad training data (see pad_corpus)
     non_polite_train_padded, polite_train_padded = pad_corpus(non_polite_train, polite_train)
 
-	#3) Pad testing data (see pad_corpus)
+	# Pad testing data (see pad_corpus)
     non_polite_test_padded, polite_test_padded = pad_corpus(non_polite_test, polite_test)
-    # print(non_polite_test_padded[0:50])
 
-	#4) Build vocab for non_polite (see build_vocab)
+	# Build vocab for non_polite (see build_vocab)
     non_polite_vocab, non_polite_pad_token_idx = build_vocab(non_polite_train_padded)
 
-	#5) Build vocab for polite (see build_vocab)
+	# Build vocab for polite (see build_vocab)
     polite_vocab, polite_pad_token_idx = build_vocab(polite_train_padded)
 
-	#6) Convert training and testing polite sentences to list of IDS (see convert_to_id)
+	# Convert training and testing polite sentences to list of IDS (see convert_to_id)
     polite_train_ids = convert_to_id(polite_vocab, polite_train_padded)
     polite_test_ids = convert_to_id(polite_vocab, polite_test_padded)
 
-	#7) Convert training and testing non_polite sentences to list of IDS (see convert_to_id)
+	# Convert training and testing non_polite sentences to list of IDS (see convert_to_id)
     non_polite_train_ids = convert_to_id(non_polite_vocab, non_polite_train_padded)
     non_polite_test_ids = convert_to_id(non_polite_vocab, non_polite_test_padded)
     
     return polite_train_ids, polite_test_ids, non_polite_train_ids, non_polite_test_ids, polite_vocab, non_polite_vocab, polite_pad_token_idx, non_polite_pad_token_idx
-
-#print("Running preprocessing...")
-# data_dir   = '../tag-and-generate-data-prep/data/catcher/'
-# file_names = ('non_polite_train', 'polite_train', 'non_polite_test', 'polite_test')
-# file_paths = [f'{data_dir}/{fname}' for fname in file_names]
-#polite_train, polite_test, non_polite_train, non_polite_test, polite_vocab, non_polite_vocab, POLITE_PADDING_INDEX = get_data()
-#print(polite_train.shape, polite_test.shape, non_polite_train.shape, non_polite_test.shape)
-#print("Preprocessing complete.")

code/rnn_model.py
@@ -3,62 +3,57 @@ import tensorflow as tf
 #from nltk.translate.bleu_score import sentence_bleu
 
 class RNN_Seq2Seq(tf.keras.Model):
-	def __init__(self, french_window_size, french_vocab_size, english_window_size, english_vocab_size):
-		###### DO NOT CHANGE ##############
+	def __init__(self, non_polite_window_size, non_polite_vocab_size, polite_window_size, polite_vocab_size):
+
 		super(RNN_Seq2Seq, self).__init__()
-		self.french_vocab_size = french_vocab_size # The size of the French vocab
-		self.english_vocab_size = english_vocab_size # The size of the English vocab
+		self.non_polite_vocab_size = non_polite_vocab_size # The size of the non-polite vocab
+		self.polite_vocab_size = polite_vocab_size # The size of the polite vocab
 
-		self.french_window_size = french_window_size # The French window size
-		self.english_window_size = english_window_size # The English window size
-		######^^^ DO NOT CHANGE ^^^##################
+		self.non_polite_window_size = non_polite_window_size # The non-polite window size
+		self.polite_window_size = polite_window_size # The polite window size
 
-		# TODO:
-		# 1) Define any hyperparameters
+		# Define hyperparameters
 		self.learning_rate = 0.001
 		self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
-
-		# Define batch size and optimizer/learning rate
 		self.batch_size = 100
-		self.embedding_size = 256
+		self.embedding_size = 256 #maybe increase
 
-		# 2) Define embeddings, encoder, decoder, and feed forward layers
-
+		# Define embeddings, encoder, decoder, and feed forward layers
 		def make_variables(*dims, initializer=tf.random.normal):
 			return tf.Variable(initializer(dims, mean=0, stddev=0.01, dtype=tf.float32))
-		self.input_embedding = make_variables(self.french_vocab_size, self.embedding_size)
-		self.output_embedding = make_variables(self.english_vocab_size, self.embedding_size)
+		self.input_embedding = make_variables(self.non_polite_vocab_size, self.embedding_size)
+		self.output_embedding = make_variables(self.polite_vocab_size, self.embedding_size)
 
-		# self.input_embedding = tf.keras.layers.Embedding(self.french_vocab_size,self.embedding_size)
-		self.encoder_gru = tf.keras.layers.GRU(200,return_sequences=True,return_state=True,recurrent_initializer='glorot_uniform')
-		# self.output_embedding = tf.keras.layers.Embedding(self.english_vocab_size, self.embedding_size)
-		self.decoder_gru = tf.keras.layers.GRU(200,return_sequences=True,return_state=True,recurrent_initializer='glorot_uniform')
+		self.encoder_gru = tf.keras.layers.GRU(200,return_sequences=True, return_state=True, recurrent_initializer='glorot_uniform')
+		self.decoder_gru = tf.keras.layers.GRU(200,return_sequences=True, return_state=True, recurrent_initializer='glorot_uniform')
 
 		self.additive_attention = tf.keras.layers.AdditiveAttention()
-		self.dense1= tf.keras.layers.Dense(self.embedding_size, activation=tf.math.tanh, use_bias=False)
-		self.densef = tf.keras.layers.Dense(self.english_vocab_size)
+		self.dense1 = tf.keras.layers.Dense(self.embedding_size, activation='relu', use_bias=False) #change use_bias to True
+		self.dense2 = tf.keras.layers.Dense(1000, activation='relu', use_bias=False)
+		self.dense3 = tf.keras.layers.Dense(4000, activation='relu', use_bias=False) #maybe decrease; doesn't need to be this big
+		self.densef = tf.keras.layers.Dense(self.polite_vocab_size)
 
 
 	@tf.function
 	def call(self, encoder_input, decoder_input, mask):
 		"""
-		:param encoder_input: batched ids corresponding to French sentences
-		:param decoder_input: batched ids corresponding to English sentences
-		:return prbs: The 3d probabilities as a tensor, [batch_size x window_size x english_vocab_size]
+		:param encoder_input: batched ids corresponding to non-polite sentences
+		:param decoder_input: batched ids corresponding to polite sentences
+		:return prbs: The 3d probabilities as a tensor, [batch_size x window_size x polite_vocab_size]
         """
 
-		# input_embeddings = self.input_embedding(encoder_input)
 		input_embeddings = tf.nn.embedding_lookup(self.input_embedding, encoder_input)
-		gru1,state1 = self.encoder_gru(input_embeddings, initial_state = None)
+		gru1, state1 = self.encoder_gru(input_embeddings, initial_state=None)
+		#gru1b, state1b = self.encoder_gru(input_embeddings, initial_state=state1)
 		output_embeddings = tf.nn.embedding_lookup(self.output_embedding, decoder_input)
-		# output_embeddings = self.output_embedding(decoder_input)
-		gru2,state2 = self.decoder_gru(output_embeddings,initial_state = state1)
+		gru2, state2 = self.decoder_gru(output_embeddings, initial_state=state1)
 		query_mask = tf.ones(tf.shape(gru2)[:-1], dtype=bool)
 		value_mask = tf.convert_to_tensor(mask, dtype=bool)
-		# print(query_mask.shape, value_mask.shape)
 		att_output = self.additive_attention([gru2,gru1], mask=[query_mask, value_mask], training=True, return_attention_scores=False)
 		dense1 = self.dense1(att_output)
-		logits = self.densef(dense1)
+		dense2 = self.dense2(dense1)
+		dense3 = self.dense3(dense2)
+		logits = self.densef(dense3)
 		prbs = tf.nn.softmax(logits)
 
 		return prbs
@@ -67,13 +62,12 @@ class RNN_Seq2Seq(tf.keras.Model):
 		"""
 		DO NOT CHANGE
 		Computes the batch accuracy
-		:param prbs:  float tensor, word prediction probabilities [batch_size x window_size x english_vocab_size]
+		:param prbs:  float tensor, word prediction probabilities [batch_size x window_size x polite_vocab_size]
 		:param labels:  integer tensor, word prediction labels [batch_size x window_size]
 		:param mask:  tensor that acts as a padding mask [batch_size x window_size]
 		:return: scalar tensor of accuracy of the batch between 0 and 1
 		"""
 
-		# decoded_symbols = tf.argmax(input=prbs, axis=2)
 		decoded_symbols = tf.cast(tf.argmax(input=prbs, axis=2), dtype=tf.float32)
 		labels = tf.cast(labels, dtype=tf.float32)
 		accuracy = tf.reduce_mean(tf.boolean_mask(tf.cast(tf.equal(decoded_symbols, labels), dtype=tf.float32),mask))
@@ -84,7 +78,7 @@ class RNN_Seq2Seq(tf.keras.Model):
 		"""
 		Calculates the total model cross-entropy loss after one forward pass.
 		Please use reduce sum here instead of reduce mean to make things easier in calculating per symbol accuracy.
-		:param prbs:  float tensor, word prediction probabilities [batch_size x window_size x english_vocab_size]
+		:param prbs:  float tensor, word prediction probabilities [batch_size x window_size x polite_vocab_size]
 		:param labels:  integer tensor, word prediction labels [batch_size x window_size]
 		:param mask:  tensor that acts as a padding mask [batch_size x window_size]
 		:return: the loss of the model as a tensor

code/tokenize_UNK.py
@@ -7,48 +7,46 @@ non_polite_train, polite_train, non_polite_test, polite_test = change_data()
 # UNK NON_POLITE TRAIN
 flat_list = [item for sublist in non_polite_train for item in sublist]
 x = Counter(flat_list)
-words_to_remove = dict((k,v) for k,v in x.items() if v==1)
-# print(len(words_to_remove))
+words_to_remove = dict((k,v) for k,v in x.items() if v<=20) #we can change this frequency threshold if needed
 omit_words = list(words_to_remove.keys())
-# print(non_polite_train_omit_words[0:100])
-# Actually replace words in non_polite_train with '*UNK*' if they appear in non_polite_train_omit_words
+# Actually replace words in non_polite_train with '*UNK*' if they appear in omit_words
 for i, sentence in np.ndenumerate(non_polite_train):
     for j, word in enumerate(sentence):
         if word in omit_words:
             non_polite_train[i][j] = '*UNK*'
-# print(non_polite_train.tolist())
 
 # UNK POLITE TRAIN
-# flat_list = [item for sublist in polite_train for item in sublist]
-# x = Counter(flat_list)
-# words_to_remove = dict((k,v) for k,v in x.items() if v==1)
-# omit_words = list(words_to_remove.keys())
-# for i, sentence in np.ndenumerate(polite_train):
-#     for j, word in enumerate(sentence):
-#         if word in omit_words:
-#             polite_train[i][j] = '*UNK*'
+flat_list = [item for sublist in polite_train for item in sublist]
+x = Counter(flat_list)
+words_to_remove = dict((k,v) for k,v in x.items() if v<=20)
+omit_words = list(words_to_remove.keys())
+for i, sentence in np.ndenumerate(polite_train):
+    for j, word in enumerate(sentence):
+        if word in omit_words:
+            polite_train[i][j] = '*UNK*'
 
 # UNK NON_POLITE TEST
-# flat_list = [item for sublist in non_polite_test for item in sublist]
-# x = Counter(flat_list)
-# words_to_remove = dict((k,v) for k,v in x.items() if v==1)
-# omit_words = list(words_to_remove.keys())
-# for i, sentence in np.ndenumerate(non_polite_test):
-#     for j, word in enumerate(sentence):
-#         if word in omit_words:
-#             non_polite_test[i][j] = '*UNK*'
+flat_list = [item for sublist in non_polite_test for item in sublist]
+x = Counter(flat_list)
+words_to_remove = dict((k,v) for k,v in x.items() if v<=20)
+omit_words = list(words_to_remove.keys())
+for i, sentence in np.ndenumerate(non_polite_test):
+    for j, word in enumerate(sentence):
+        if word in omit_words:
+            non_polite_test[i][j] = '*UNK*'
 
 # UNK POLITE TEST
-# flat_list = [item for sublist in polite_test for item in sublist]
-# x = Counter(flat_list)
-# words_to_remove = dict((k,v) for k,v in x.items() if v==1)
-# omit_words = list(words_to_remove.keys())
-# for i, sentence in np.ndenumerate(polite_test):
-#     for j, word in enumerate(sentence):
-#         if word in omit_words:
-#             polite_test[i][j] = '*UNK*'
+flat_list = [item for sublist in polite_test for item in sublist]
+x = Counter(flat_list)
+words_to_remove = dict((k,v) for k,v in x.items() if v<=20)
+omit_words = list(words_to_remove.keys())
+for i, sentence in np.ndenumerate(polite_test):
+    for j, word in enumerate(sentence):
+        if word in omit_words:
+            polite_test[i][j] = '*UNK*'
 
-# SAVE 4 NEW FILES
-# file1 = open('../tag-and-generate-data-prep/data/catcher/tokenized_non_polite_train.txt', "w+")
-# file1.write(str(non_polite_train))
-# file1.close()
+# SAVE NEW FILES
+np.savetxt('tokenized_non_polite_train_20.txt', non_polite_train, fmt='%s')
+np.savetxt('tokenized_polite_train_20.txt', polite_train, fmt='%s')
+np.savetxt('tokenized_non_polite_test_20.txt', non_polite_test, fmt='%s')
+np.savetxt('tokenized_polite_test_20.txt', polite_test, fmt='%s')